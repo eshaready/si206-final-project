f40a453db65eeb85b7e260272d0f5f847baf9c36
papapizzachess <128561666+papapizzachess@users.noreply.github.com>
2023-12-07 13:09:21 -0500

    edited line of best fit

--- average_precip_box_office_graph.py
+++ average_precip_box_office_graph.py
@@ -17,9 +17,10 @@ movie_gross = [row[0] for row in data]
 average_precip = [row[1] for row in data]
 
 # Plotting the scatter plot
-a, b = np.polyfit(movie_gross, average_precip, 1)
+coefficients = np.polyfit(movie_gross, average_precip, 1)
+line_of_best_fit = np.polyval(coefficients, movie_gross)
 plt.scatter(movie_gross, average_precip)
-plt.plot(movie_gross, a*movie_gross+b)
+plt.plot(movie_gross, line_of_best_fit)
 plt.title('Correlation between Movie Gross and Average Precipitation')
 plt.xlabel('Movie Gross')
 plt.ylabel('Average Precipitation')
